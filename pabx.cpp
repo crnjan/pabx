13a674810e4fb1530efcff329612b1dc6f5f3c50
Boris Krivonog <boris.krivonog@inova.si>
2021-08-31 00:18:59 +0200

    fixed flow

--- pabx.cpp
+++ pabx.cpp
@@ -2,12 +2,11 @@
 #include <cstring>
 #include "recognizer.h"
 
-typedef enum { IDLE, IN_CALL } StateType;
 
-// 11, 12, 13, 14 = pins used for ring selectors
 //
 // Raspberry Pi Model B Rev 1
 // 
+
 // 0 -> 11 (header)
 // 1 -> 12 (header)
 // 2 -> 13 (header)
@@ -23,22 +22,27 @@ static const int LINE_SENSE_PIN = 4;
 // 5 -> 18 (header)
 static const int RINGER_POWERED_PIN = 5;
 
-// 8 -> 3 (header)
-// 9 -> 5 (header)
-static const int RING_GENERATOR_PIN = 8;
-static const int RING_GENERATOR_INVERTED_PIN = 9;
+// 12 -> 19 (header)
+// 13 -> 21 (header)
+static const int RING_GENERATOR_PIN = 12;
+static const int RING_GENERATOR_INVERTED_PIN = 13;
+
+
+typedef enum { IDLE, IN_CALL } StateType;
+
 
 static void ring(int ringPhonePin) {
+  // Make sure power MOSFETs are tured off.
   digitalWrite(RING_GENERATOR_PIN, HIGH);
   digitalWrite(RING_GENERATOR_INVERTED_PIN, HIGH);
   delay(10);
 
+  // Turn ringer circuit on.
   digitalWrite(RINGER_POWERED_PIN, HIGH);
-  delay(100);
-
   digitalWrite(ringPhonePin, HIGH);
   delay(100);
 
+  // Generate ringing for 2.5s
   for (int i = 0; i < 50; ++i) {
       digitalWrite(RING_GENERATOR_PIN, HIGH);
       digitalWrite(RING_GENERATOR_INVERTED_PIN, LOW);
@@ -48,15 +52,18 @@ static void ring(int ringPhonePin) {
       delay(25);
   }
 
+  // Turn power MOSFETs off.
   digitalWrite(RING_GENERATOR_PIN, HIGH);
   digitalWrite(RING_GENERATOR_INVERTED_PIN, HIGH);
   delay(10);
 
+  // Turn ringer circuit off.
+  digitalWrite(ringPhonePin, LOW);
   digitalWrite(RINGER_POWERED_PIN, LOW);
   delay(100);
 
-  digitalWrite(ringPhonePin, HIGH);
-  delay(100);
+  digitalWrite(RING_GENERATOR_PIN, LOW);
+  digitalWrite(RING_GENERATOR_INVERTED_PIN, LOW);
 }
 
 static void processNumber(const char* phoneNumber) {
