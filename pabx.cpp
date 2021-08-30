733cafc7aec8ad0daebb5c5823a5271caa29d2ea
Boris Krivonog <boris.krivonog@inova.si>
2021-08-30 18:34:41 +0200

    Ringer implemented ...

--- pabx.cpp
+++ pabx.cpp
@@ -20,10 +20,43 @@ static const int RING_PHONE_4_PIN = 3;
 // 4 -> 16 (header)
 static const int LINE_SENSE_PIN = 4;
 
+// 5 -> 18 (header)
+static const int RINGER_POWERED_PIN = 5;
+
+// 8 -> 3 (header)
+// 9 -> 5 (header)
+static const int RING_GENERATOR_PIN = 8;
+static const int RING_GENERATOR_INVERTED_PIN = 9;
+
 static void ring(int ringPhonePin) {
+  digitalWrite(RING_GENERATOR_PIN, HIGH);
+  digitalWrite(RING_GENERATOR_INVERTED_PIN, HIGH);
+  delay(10);
+
+  digitalWrite(RINGER_POWERED_PIN, HIGH);
+  delay(100);
+
   digitalWrite(ringPhonePin, HIGH);
-  delay(3000);
-  digitalWrite(ringPhonePin, LOW);
+  delay(100);
+
+  for (int i = 0; i < 50; ++i) {
+      digitalWrite(RING_GENERATOR_PIN, HIGH);
+      digitalWrite(RING_GENERATOR_INVERTED_PIN, LOW);
+      delay(25);
+      digitalWrite(RING_GENERATOR_INVERTED_PIN, HIGH);
+      digitalWrite(RING_GENERATOR_PIN, LOW);
+      delay(25);
+  }
+
+  digitalWrite(RING_GENERATOR_PIN, HIGH);
+  digitalWrite(RING_GENERATOR_INVERTED_PIN, HIGH);
+  delay(10);
+
+  digitalWrite(RINGER_POWERED_PIN, LOW);
+  delay(100);
+
+  digitalWrite(ringPhonePin, HIGH);
+  delay(200);
 }
 
 static void processNumber(const char* phoneNumber) {
@@ -45,11 +78,17 @@ static void processNumber(const char* phoneNumber) {
 int main() {
   wiringPiSetup();
 
+  pinMode(RINGER_POWERED_PIN, OUTPUT);
+  pinMode(RING_GENERATOR_INVERTED_PIN, OUTPUT);
+  pinMode(RING_GENERATOR_PIN, OUTPUT);
   pinMode(RING_PHONE_1_PIN, OUTPUT);
   pinMode(RING_PHONE_2_PIN, OUTPUT);
   pinMode(RING_PHONE_3_PIN, OUTPUT);
-  pinMode(RING_PHONE_4_PIN, OUTPUT);
+  pinMode(RING_PHONE_4_PIN, OUTPUT);  
 
+  digitalWrite(RINGER_POWERED_PIN, LOW);
+  digitalWrite(RING_GENERATOR_INVERTED_PIN, LOW);
+  digitalWrite(RING_GENERATOR_PIN, LOW);
   digitalWrite(RING_PHONE_1_PIN, LOW);
   digitalWrite(RING_PHONE_2_PIN, LOW);
   digitalWrite(RING_PHONE_3_PIN, LOW);
